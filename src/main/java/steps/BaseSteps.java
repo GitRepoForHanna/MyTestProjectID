bb0b9da05343b49df37815ff291307451cf8fd50
GitRepoForHanna <Annette95@yandex.ru>
2019-09-05 16:31:15 +0300

    loggers are added

--- src/main/java/steps/BaseSteps.java
+++ src/main/java/steps/BaseSteps.java
@@ -1,8 +1,11 @@
 package steps;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import pages.BasePage;
 
 public class BaseSteps {
+    public static final Logger LOGGER = LogManager.getLogger(CarPageSteps.class);
 
 //    public BasePage getCurrentPage() {
 //
--- src/main/java/steps/CarPageSteps.java
+++ src/main/java/steps/CarPageSteps.java
@@ -1,6 +1,8 @@
 package steps;
 
 import io.qameta.allure.Step;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import pages.car_rent.CarPage;
 import pages.date.DateCalendarPage;
 
@@ -11,6 +13,7 @@ public class CarPageSteps extends BaseSteps {
 
     @Step("Set the checkIn point for car renting: {0} - destination in short, {1} - full name of destination")
     public void setDestination(String destinationShort, String destination) throws InterruptedException {
+        LOGGER.info(String.format("Set checkIn point on Car Renting page: %s - destination in short, %s - full name destination", destinationShort, destination));
         CarPage carPage = new CarPage();
         carPage.setDestination(destinationShort);
         carPage.selectDestinationOption(destination);
@@ -18,6 +21,7 @@ public class CarPageSteps extends BaseSteps {
 
     @Step("Set {0} as checkIn date for car renting")
     public void setRentingDateFrom(LocalDate dateFrom) throws InterruptedException {
+        LOGGER.info("Set checkIn date on Car Renting page");
         CarPage carPage = new CarPage();
         carPage.clickOnDateFromInput();
         new DateCalendarPage().setDateFrom(dateFrom);
@@ -27,12 +31,14 @@ public class CarPageSteps extends BaseSteps {
 
     @Step("Set {0} as checkOut date for car renting")
     public void setRentingDateTo(LocalDate dateTo) throws InterruptedException{
+        LOGGER.info("Set checkOut date on Car Renting page");
         new CarPage().clickOnDateToInput();
         new DateCalendarPage().setDateTo(dateTo);
     }
 
     @Step("Click button Search on Car page")
     public void clickSearchButton() {
+        LOGGER.info("Click Search button on Car Renting page");
         new CarPage().clickSearchButton();
     }
 }
