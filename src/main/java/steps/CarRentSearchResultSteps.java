d0c9352ee19d00f11012ec2bbc2d323113566488
GitRepoForHanna <Annette95@yandex.ru>
2019-09-18 09:53:27 +0300

    added Logger in the step which fills the Car Configuration form with business object Car

--- src/main/java/steps/CarRentSearchResultSteps.java
+++ src/main/java/steps/CarRentSearchResultSteps.java
@@ -8,9 +8,6 @@ import org.openqa.selenium.WebDriver;
 import pages.car_rent.CarRentSearchResultPage;
 import utils.webdriver.JSWaiter;
 import utils.webdriver.WebDriverSingletoneInstance;
-
-import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.List;
 
 public class CarRentSearchResultSteps {
@@ -19,66 +16,6 @@ public class CarRentSearchResultSteps {
     public void setCarCategory(String category) {
         Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Car Category %s", category));
         new CarRentSearchResultPage().selectCarCategory(category);
-    }
-
-    @Step("Set Suppliers {0}")
-    public void setSuppliers(List<SuppliersOptions> suppliers) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Car Suppliers %s", suppliers));
-        setOptions(suppliers);
-    }
-
-    @Step("Set Party Size {0}")
-    public void setPartySize(List<PartySize> partySizes) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Car Party Size %s", partySizes));
-        setOptions(partySizes);
-    }
-
-    @Step("Set Payment Cards {0}")
-    public void setPaymentCard(List<PaymentCard> cards) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Payment Cards %s", cards));
-        setOptions(cards);
-     }
-
-    @Step("Set Supplier Raitings {0}")
-    public void setSuppliersRaititng(List<SupplierRating> raitings) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Supplier Raitings %s", raitings));
-        setOptions(raitings);
-        }
-
-    @Step("Set Supplier Location {0}")
-    public void setSuppliersLocation(List<SupplierLocation> locations) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Suppliers Locations %s", locations));
-        setOptions(locations);
-    }
-
-    @Step("Set Car Specification {0}")
-    public void setCarSpecifications(List<CarSpecification> specifications) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Car Specifications %s", specifications));
-        setOptions(specifications);
-    }
-
-    @Step("Set Car Kilometres {0}")
-    public void setCarKilometres(List<Kilometres> kilometres) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Car Kilometres %s", kilometres));
-        setOptions(kilometres);
-    }
-
-    @Step("Set Fuel Options {0}")
-    public void setFuelOptions(List<FuelPolicy> fuelPolicies) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Fuel Options %s", fuelPolicies));
-        setOptions(fuelPolicies);
-    }
-
-    @Step("Set Damage Excess {0}")
-    public void setDamageExcess(List<DamageExcess> damageExcess) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Damage Excess  %s", damageExcess));
-        setOptionsByIndex(damageExcess);
-    }
-
-    @Step("Set Deposit At Pick Up {0}")
-    public void setDepositAtPickUp(List<DepositAtPickUp> deposits) {
-        Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Deposit At Pick Up  %s", deposits));
-        setOptionsByIndex(deposits);
     }
 
     public void setOptions(List<? extends ICheckBoxGroup> options) {
@@ -99,45 +36,66 @@ public class CarRentSearchResultSteps {
                 .forEach(searchResultPage::checkOptionByIndex);
     }
 
+    @Step("Set Car Configuration {0}")
     public void setCarConfiguration(CarBuilder.Car carConfiguration) {
         if(carConfiguration.getSuppliers() != null) {
-            setOptions(carConfiguration.getSuppliers());
+            List<SuppliersOptions> suppliers = carConfiguration.getSuppliers();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Car Suppliers %s", suppliers));
+            setOptions(suppliers);
         }
 
         if(carConfiguration.getSpecifications() != null) {
-            setOptions(carConfiguration.getSpecifications());
+            List<CarSpecification> specifications = carConfiguration.getSpecifications();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Car Specifications %s", specifications));
+            setOptions(specifications);
         }
 
         if(carConfiguration.getSupplierLocations() != null) {
-            setOptions(carConfiguration.getSupplierLocations());
+            List<SupplierLocation> locations = carConfiguration.getSupplierLocations();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Supplier Location %s", locations));
+            setOptions(locations);
         }
 
         if(carConfiguration.getSupplierRatings() != null) {
-            setOptions(carConfiguration.getSupplierRatings());
+            List<SupplierRating> ratings = carConfiguration.getSupplierRatings();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Supplier Raiting %s", ratings));
+            setOptions(ratings);
         }
 
         if(carConfiguration.getPartySizes() != null) {
-            setOptions(carConfiguration.getPartySizes());
+            List<PartySize> partySizes = carConfiguration.getPartySizes();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Party Size %s", partySizes));
+            setOptions(partySizes);
         }
 
         if(carConfiguration.getFuelPolicies() != null) {
-            setOptions(carConfiguration.getFuelPolicies());
+            List<FuelPolicy> policies = carConfiguration.getFuelPolicies();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Fuel Policies %s", policies));
+            setOptions(policies);
         }
 
         if(carConfiguration.getKilometres() != null) {
-            setOptions(carConfiguration.getKilometres());
+            List<Kilometres> kilometres = carConfiguration.getKilometres();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Kilometres %s", kilometres));
+            setOptions(kilometres);
         }
 
         if(carConfiguration.getPaymentCards() != null) {
-            setOptions(carConfiguration.getPaymentCards());
+            List<PaymentCard> paymentCards = carConfiguration.getPaymentCards();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Payment Cards %s", paymentCards));
+            setOptions(paymentCards);
         }
 
         if(carConfiguration.getDepositAtPickUps() != null) {
-            setOptionsByIndex(carConfiguration.getDepositAtPickUps());
+            List<DepositAtPickUp> deposits = carConfiguration.getDepositAtPickUps();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Deposits At Pick Up %s", deposits));
+            setOptionsByIndex(deposits);
         }
 
         if(carConfiguration.getDamageExcesses() != null) {
-            setOptionsByIndex(carConfiguration.getDamageExcesses());
+            List<DamageExcess> excesses = carConfiguration.getDamageExcesses();
+            Logger.getLogger(CarRentSearchResultSteps.class).info(String.format("Set Damage Excesses %s", excesses));
+            setOptionsByIndex(excesses);
         }
     }
 
